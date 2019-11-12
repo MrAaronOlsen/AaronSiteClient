dc5f6dfc45762f01f56eb1fb3f621079ffdf22ec
Aaron Olsen <mraaronolsen@gmail.com>
2019-11-12 06:43:06 -0700

    This is frustrating...

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
+app.use(express.static(path.join(__dirname)));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
