8871b4601b176e8c0e33dfd071ea82d354719958
Aaron Olsen <mraaronolsen@gmail.com>
2019-11-13 06:55:58 -0700

    Dont feel like setting up a server webpack yet

--- server.js
+++ server.js
@@ -29,23 +29,20 @@ var server = app.listen(app.get('port'), function() {
 // Keep this server alive
 const fetch = require('isomorphic-fetch')
 
-function() {
+setInterval(function() {
+  fetch("https://aaron-site.herokuapp.com/keepalive")
+    .then(response => {
+      return response.text();
+    })
+    .then(data => {
+      console.log("[Keep Alive] Client: " + data);
+    })
 
-  setInterval(function() {
-    fetch("https://aaron-site.herokuapp.com/keepalive")
-      .then(response => {
-        return response.text();
-      })
-      .then(data => {
-        console.log("[Keep Alive] Client: " + data);
-      })
-
-    fetch("https://aaron-site-api.herokuapp.com/aaron_site/api/v1/keepalive")
-      .then(response => {
-        return response.text();
-      })
-      .then(data => {
-        console.log("[Keep Alive] API: " + data);
-      })
-  }, 30000);
-}
+  fetch("https://aaron-site-api.herokuapp.com/aaron_site/api/v1/keepalive")
+    .then(response => {
+      return response.text();
+    })
+    .then(data => {
+      console.log("[Keep Alive] API: " + data);
+    })
+}, 30000);
