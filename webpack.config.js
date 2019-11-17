1b5591e3a1ba06ec9457f47e1f1b0ed807c77250
Aaron Olsen <mraaronolsen@gmail.com>
2019-11-17 09:42:52 -0700

    Merged dev and prod configs with common. Much better.

--- webpack.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = (env) => {
-  console.log("[INFO] Working in " + env + "environment.")
-  return require(`./webpack.${env}.js`)
-}
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,76 +1,11 @@
 const path = require("path");
-const webpack = require("webpack");
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-  entry: ['./src/index.js'],
+console.log("[INFO] Using dev config.")
+
+module.exports = merge(common, {
   mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        include: /src/,
-        options: {
-          presets: ["@babel/env"],
-          plugins: ['@babel/plugin-proposal-class-properties']
-        }
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          "style-loader",
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: true
-            }
-          },
-          "sass-loader"
-        ],
-        include: /\.mod\.scss$/
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          "style-loader",
-          'css-loader',
-          "sass-loader"
-        ],
-        exclude: /\.mod\.scss$/
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: '/images'
-            },
-          },
-        ],
-      }
-    ]
-  },
-  resolve: {
-    extensions: ["scss", ".js", ".jsx"],
-    alias: {
-      public: path.resolve(__dirname, 'public'),
-      mixins: path.resolve(__dirname, 'src/mixins'),
-      colors: path.resolve(__dirname, 'src/constants/colors.scss'),
-      modules: path.resolve(__dirname, 'src/modules'),
-      http: path.resolve(__dirname, 'src/http'),
-      auth: path.resolve(__dirname, 'src/auth'),
-      logger$: path.resolve(__dirname, 'src/utils/Logger.js'),
-    }
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/public",
-    filename: "bundle.js"
-  },
   devServer: {
     contentBase: path.resolve(__dirname, "public"),
     publicPath: "/dist",
@@ -80,7 +15,4 @@ module.exports = {
     historyApiFallback: true
   },
   devtool : '#eval-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
-};
+})
