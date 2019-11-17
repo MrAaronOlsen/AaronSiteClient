03f4a2d5d8f77c73467c8ddc173519da884b06a2
Aaron Olsen <mraaronolsen@gmail.com>
2019-11-17 08:35:17 -0700

    Force HTTPS

--- src/http/url.js
+++ src/http/url.js
@@ -1,15 +1,13 @@
 const dev = 'http://localhost:8080/'
 const prod = 'https://aaron-site-api.herokuapp.com/'
 
-var envUrl;
+var envUrl = dev;
 
 if (process.env.NODE_ENV === 'production') {
   envUrl = prod;
-} else {
-  envUrl = dev;
 }
 
-console.log("[INFO] Using url " + envUrl);
+console.log("[INFO] Using API url " + envUrl);
 
 export const BASE_URL = envUrl;
 export const API_V1 = 'aaron_site/api/v1/';
