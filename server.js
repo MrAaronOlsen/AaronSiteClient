043ae368648060d90fb027cdc3f4331d4a6ec543
Aaron Olsen <mraaronolsen@gmail.com>
2019-11-17 09:06:14 -0700

    Fixed force to HTTPS issue with cors

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const express = require('express');
 
 var app = express();
 
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
+// Force all http requests to server to be secured https.
+// This is needed because cors policy on the API only accepts https:<site> as origin.
+function forceHTTPS(req, res, next) {
+  // Heroku ovewrites x-forwarded-proto with the originating protocol of the HTTP request.
   if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
     return res.redirect('https://' + req.get('host') + req.url);
   }
@@ -14,14 +16,15 @@ function requireHTTPS(req, res, next) {
   next();
 }
 
-app.use(requireHTTPS);
+// Inject the force https function as middleware.
+app.use(forceHTTPS);
 
-// Sets the root.
+// Let's express serve up static files from our root. Needed to make React Routes work.
 app.use(express.static(path.join(__dirname)));
 
-// Used to keep this server alive on heroku
-app.get("/keepalive", function(req, res) {
-  res.send("I'm alive")
+// Special endpoint to keep client server from going to sleep.
+app.get("/keepawake", function(req, res) {
+  res.send("I'm awake!")
 });
 
 // This routes all React Routes to our index.html, which in turn always calls bundle.js.
@@ -29,29 +32,31 @@ app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+// If we're passed a port bind it, otherwise use 3000
 app.set('port', process.env.PORT || 3000);
 
+// Create the server and put its ear to the binded port.
 var server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
+  console.log('[INFO] Listening on port ', server.address().port);
 });
 
-// Keep this server alive
+// Function to keep our servers from sleeping. Calls both the client and api servers. Calls every 5 mins.
 const fetch = require('isomorphic-fetch')
 
 setInterval(function() {
-  fetch("https://aaron-site.herokuapp.com/keepalive")
+  fetch("https://aaron-site.herokuapp.com/keepawake")
     .then(response => {
       return response.text();
     })
     .then(data => {
-      console.log("[Keep Alive] Client: " + data);
+      console.log("[Keep Awake] Client: " + data);
     })
 
-  fetch("https://aaron-site-api.herokuapp.com/aaron_site/api/v1/keepalive")
+  fetch("https://aaron-site-api.herokuapp.com/aaron_site/api/v1/keepawake")
     .then(response => {
       return response.text();
     })
     .then(data => {
-      console.log("[Keep Alive] API: " + data);
+      console.log("[Keep Awake] API: " + data);
     })
 }, 300000);
