fd38634b10585b4624c5ab6fa57983bf612eb913
Aaron Olsen <mraaronolsen@gmail.com>
2019-12-03 21:04:35 -0700

    Fixed some bad cased file names

--- src/app/main/pages/stack/Stack.jsx
+++ src/app/main/pages/stack/Stack.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 
+import Blocks from 'blocks/Blocks.jsx'
+import ArrowBtn from 'modules/buttons/ArrowBtn.jsx'
+
 import styles from './stack.mod.scss'
 
 export default class Stack extends Component {
 
+  onClick() {
+    this.props.history.push('/pages')
+  }
+  
   render() {
     return(
       <div className={styles.wrapper}>
-        Stack.
+        <ArrowBtn classNames={styles.button} onClick={this.onClick.bind(this)} />
+        <Blocks query="pages?slug=stack"/>
       </div>
     )
   }
