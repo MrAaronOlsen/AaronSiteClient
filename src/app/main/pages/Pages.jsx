3e556fc02d6b4ff12d6fcc30147a945b9d006a77
Aaron Olsen <mraaronolsen@gmail.com>
2019-12-10 20:43:38 -0700

    Pages use single component and some css refactoring.

--- src/app/main/pages/Pages.jsx
+++ src/app/main/pages/Pages.jsx
@@ -2,9 +2,7 @@ import React, { Component } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 import List from './list/List.jsx'
-import About from './about/About.jsx'
-import Portfolio from './portfolio/Portfolio.jsx'
-import Stack from './stack/Stack.jsx'
+import Page from './page/Page.jsx'
 
 import styles from './pages.mod.scss'
 
@@ -15,9 +13,9 @@ export default class Pages extends Component {
     return(
         <div className={styles.wrapper}>
           <Switch>
-            <Route path='/pages/about' component={About} />
-            <Route path='/pages/portfolio' component={Portfolio} />
-            <Route path='/pages/stack' component={Stack} />
+            <Route path='/pages/about' component={ (props) => <Page {...props} query='pages?slug=about' /> } />
+            <Route path='/pages/portfolio' component={ (props) => <Page {...props} query='pages?slug=portfolio' /> } />
+            <Route path='/pages/stack' component={ (props) => <Page {...props} query='pages?slug=stack' /> } />
             <Route path='/pages' component={List} />
 
             <Redirect to='/pages' />
