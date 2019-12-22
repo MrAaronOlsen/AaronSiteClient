a00c95f24972fd2df29b586194ad8cf3a9e063a5
Aaron Olsen <mraaronolsen@gmail.com>
2019-12-21 18:48:53 -0700

    save

--- src/app/admin/pages/pageform/menubar/MenuBar.jsx
+++ src/app/admin/pages/pageform/menubar/MenuBar.jsx
@@ -8,21 +8,25 @@ export default function MenuBar(props) {
 
   return(
     <div className={ styles.wrapper }>
-      <ActionBtn text="Save"
-        classNames={styles.button}
-        onClick={props.save} />
-
-      <ActionBtn text="New"
-        classNames={styles.button}
-        onClick={props.new} />
-
       <PageList
         pages={props.pages}
         focus={props.focus} />
 
-      <ActionBtn text="delete"
+      <ActionBtn text="Save"
+        classNames={styles.button}
+        onClick={props.save} />
+
+      <ActionBtn text="Delete"
           classNames={styles.button}
           onClick={props.delete} />
+
+      <ActionBtn text="Check Out"
+        classNames={styles.button}
+        onClick={{}} />
+
+      <ActionBtn text="New"
+        classNames={styles.button}
+        onClick={props.new} />
     </div>
   )
 }
