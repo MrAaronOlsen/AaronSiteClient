bfde6d3b905d84f2360dae0faaa77288f236d79c
Aaron Olsen <aaron.olsen@nextworld.net>
2020-02-03 20:58:48 -0700

    Blocks no longer do queries and added header t

--- src/app/main/pages/page/Page.jsx
+++ src/app/main/pages/page/Page.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
+import Logger from 'logger';
+import { API_V1 } from 'http/url.js';
+import { GET } from 'http/get.js';
+
 import Blocks from 'blocks/Blocks.jsx'
 import Transition from 'modules/transition/Transition.jsx'
 import ArrowBtn from 'modules/buttons/ArrowBtn.jsx'
@@ -15,10 +19,36 @@ const arrowTransitionConfig = {
   transDurationIn: '1000ms'
 }
 
+const arrowStyles = {
+  'position': 'absolute',
+  'width': 'auto',
+  'z-index': '999'
+}
+
+const headerTransitionConfig = {
+  transProperty: 'opacity',
+  startValue: '0',
+  inValue: '1',
+  outValue: '0',
+  transDurationIn: '1000ms'
+}
+
+const headerStyles = {
+  display: 'flex',
+  'justify-content': 'center',
+  'align-items': 'center',
+  width: '100%',
+  'font-size': '24px'
+}
+
 export default class Page extends Component {
   state = {
     redirect: false,
     triggerOut: false
+  }
+
+  componentDidMount() {
+    this.load()
   }
 
   redirect() {
@@ -33,25 +63,60 @@ export default class Page extends Component {
     })
   }
 
+  page() {
+    return this.state.page || {}
+  }
+
+  load() {
+    const query = 'pages/' + this.props.location.state.id
+
+    GET(API_V1 + query, (payload) => {
+      if (payload.hasErrors()) {
+        Logger.error("Failed to load page. Cause: " + payload.getErrors());
+      } else {
+        this.setState({
+          page: payload.getFirst()
+        })
+      }
+    })
+  }
+
   render() {
     if (this.state.redirect) {
       return <Redirect to='/pages' />
     }
 
+    if (!this.state.page) {
+      return null
+    }
+
     return(
       <div className={styles.wrapper}>
         <div className={styles.header}>
-          <Transition config={arrowTransitionConfig}
+          <Transition
+            styles={arrowStyles}
+            config={arrowTransitionConfig}
             outTrigger={this.state.triggerOut}>
 
-            <ArrowBtn classNames={styles.button} direction={'left'} onClick={this.triggerOut.bind(this)} />
+            <ArrowBtn classNames={styles.button}
+              direction={'left'}
+              onClick={this.triggerOut.bind(this)} />
+            
+          </Transition>
+          <Transition
+            styles={headerStyles}
+            config={headerTransitionConfig}
+            outTrigger={this.state.triggerOut}>
+
+            {this.page().header}
           </Transition>
         </div>
         <div className={styles.blocks}>
           <Blocks
             triggerOut={this.state.triggerOut}
             actionOut={this.redirect.bind(this)}
-            query={'pages/' + this.props.location.state.id}/>
+            blocks={this.page().blocks}
+            setState={this.setState.bind(this)}/>
         </div>
       </div>
     )
--- src/modules/blocks/Blocks.jsx
+++ src/modules/blocks/Blocks.jsx
@@ -1,35 +1,10 @@
 import React, { Component } from 'react'
 
 import BlockIterator from './BlockIterator.jsx'
-import Logger from 'logger';
-
-import { API_V1 } from 'http/url.js';
-import { GET } from 'http/get.js';
 
 import styles from './blocks.mod.scss'
 
 export default class Blocks extends Component {
-  state = {
-    blocks: {}
-  }
-
-  componentDidMount() {
-    if (this.props.query) {
-      this.load()
-    }
-  }
-
-  load() {
-    GET(API_V1 + this.props.query, (payload) => {
-      if (payload.hasErrors()) {
-        Logger.error("Failed to load page. Cause: " + payload.getErrors());
-      } else {
-        this.setState({
-          blocks: payload.getFirst().blocks
-        })
-      }
-    })
-  }
 
   render() {
     return(
@@ -38,7 +13,7 @@ export default class Blocks extends Component {
             triggerOut={this.props.triggerOut}
             triggerAction={this.props.actionOut}
             start={'start'}
-            blocks={this.props.blocks || this.state.blocks} />
+            blocks={this.props.blocks || {}} />
         </div>
     )
   }
